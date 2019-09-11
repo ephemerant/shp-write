2007a1efbf983f4dd9b9cf8e4e07a3c2a335e00d
Ryan Sims <dulldrums09@gmail.com>
2019-09-11 18:03:57 -0400

    add point and multipoint tests
    add LineString tests, skip Z shape tests

--- test/geojson.test.js
+++ test/geojson.test.js
@@ -39,6 +39,142 @@ describe('geojson', function() {
         });
     });
     
+    describe('#pointZ', function() {
+        it.skip('should return coordinates, properties, and type', function() {
+            const result = geojson.pointZ(gjPointZ);
+            
+            const idealResult = {
+                geometries: [
+                    [
+                        [2.2945064306259155, 48.85825817805569, 75],
+                    ]
+                ],
+                properties: [{
+                    name: 'PointZ-Name'
+                }],
+                type: 'POINTZ'
+            };
+            
+            expect(result).to.eql(idealResult);
+        });
+    });
+    
+    describe('#multipoint', function() {
+        it('should return coordinates, properties, and type', function() {
+            const result = geojson.multipoint(gjMultiPoint);
+            
+            const idealResult = {
+                geometries: [
+                    [
+                        [
+                            2.2945064306259155,
+                            48.85825817805569
+                        ],
+                        [
+                            2.312525510787964,
+                            48.854991630542706
+                        ]
+                    ]
+                ],
+                properties: [{
+                    name: 'MultiPoint-Name'
+                }],
+                type: 'MULTIPOINT'
+            };
+            
+            expect(result).to.eql(idealResult);
+        });
+    });
+    
+    describe('#multipointZ', function() {
+        it.skip('should return coordinates, properties, and type', function() {
+            const result = geojson.multipointZ(gjMultiPointZ);
+            
+            const idealResult = {
+                geometries: [
+                    [
+                        [
+                            2.2945064306259155,
+                            48.85825817805569,
+                            75
+                        ],
+                        [
+                            2.312525510787964,
+                            48.854991630542706,
+                            5
+                        ]
+                    ]
+                ],
+                properties: [{
+                    name: 'MultiPointZ-Name'
+                }],
+                type: 'MULTIPOINTZ'
+            };
+            
+            expect(result).to.eql(idealResult);
+        });
+    });
+    
+    describe('#linestring', function() {
+        it('should return coordinates, properties, and type', function() {
+            const result = geojson.line(gjLineString);
+            
+            const idealResult = {
+                geometries: [
+                    [
+                        [
+                            [
+                                -120.861132144928,
+                                37.52547739205533
+                            ],
+                            [
+                                -120.85253834724426,
+                                37.525460374347695
+                            ]
+                        ]
+                    ]
+                ],
+                properties: [{
+                    name: 'LineString-Name'
+                }],
+                type: 'POLYLINE'
+            };
+            
+            expect(result).to.eql(idealResult);
+        });
+    });
+    
+    describe('#linestringZ', function() {
+        it.skip('should return coordinates, properties, and type', function() {
+            const result = geojson.line(gjLineStringZ);
+            
+            const idealResult = {
+                geometries: [
+                    [
+                        [
+                            [
+                                -120.861132144928,
+                                37.52547739205533,
+                                200
+                            ],
+                            [
+                                -120.85253834724426,
+                                37.525460374347695,
+                                250
+                            ]
+                        ]
+                    ]
+                ],
+                properties: [{
+                    name: 'LineStringZ-Name'
+                }],
+                type: 'POLYLINEZ'
+            };
+            
+            expect(result).to.eql(idealResult);
+        });
+    });
+    
     describe('#polygon', function() {
         it('should return coordinates, properties, and type', function() {
             const result = geojson.polygon(gjPolygon);
@@ -61,6 +197,34 @@ describe('geojson', function() {
                     name: 'Polygon-Name'
                 }],
                 type: 'POLYGON'
+            };
+  
+            expect(result).to.eql(idealResult);
+        });
+    });
+    
+    describe('#polygonZ', function() {
+        it.skip('should return coordinates, properties, and type', function() {
+            const result = geojson.polygonZ(gjPolygonZ);
+            
+            const idealResult = {
+                geometries: [
+                    [
+                        [
+                            [
+                                [-122.30279803276056, 37.88183985973943, 100],
+                                [-122.3025619983673, 37.88112007262953, 110],
+                                [-122.30207920074463, 37.88123862628449, 120],
+                                [-122.3023045063018, 37.881933008145445, 130],
+                                [-122.30279803276056, 37.88183985973943, 100]
+                            ]
+                        ]
+                    ],
+                ],
+                properties: [{
+                    name: 'PolygonZ-Name'
+                }],
+                type: 'POLYGONZ'
             };
   
             expect(result).to.eql(idealResult);
